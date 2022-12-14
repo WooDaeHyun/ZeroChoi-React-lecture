8841ec373fe0a20ea1f84f7dcaf2e2f9e9fd173f
WooDaeHyun <po668312@naver.com>
2022-12-15 00:43:00 +0900

    feat: WordRelay Game 기본 동작 구현

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -4,11 +4,58 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "Hello, webpack",
+    word: "우대현",
+    value: "",
+    result: "",
+  };
+  //클래스의 내가 만드는 메서드는 반드시 화살표 함수로 해주어야한다!!
+  //render처럼 직접 있는것은 빼고
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: "딩동댕",
+        word: this.state.value,
+        value: "",
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: "땡",
+        value: "",
+      });
+      //this.setState에 업데이트에 필요한 데이터만 넣어도 기존에 있던 state가 새롭게 할당되는게 아니라 기본 js 객체 문법과 동일함
+      console.log(this.state);
+      this.input.focus();
+    }
   };
 
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
+  };
+
+  //지금은 class형태 이므로 state의 데이터를 활용할때 this.state.으로 데이터를 불러와야함!!
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
