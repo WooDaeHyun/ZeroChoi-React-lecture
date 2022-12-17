76b16372e2296adc1a3c2ef66eef7bf264e3b49d
WooDaeHyun <po668312@naver.com>
2022-12-18 00:40:15 +0900

    feat: Lotto추첨기 class 컴포넌트로 구현

--- 6.로또/Lotto.jsx
+++ 6.로또/Lotto.jsx
@@ -1,12 +1,94 @@
 import React, { useState, useRef, Component } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((a, b) => a - b);
+  return [...winNumbers, bonusNumber];
+}
 
 class Lotto extends Component {
-  state = {};
+  state = {
+    winNumbers: getWinNumbers(), // 당첨 숫자들
+    winBalls: [], // 당첨 번호들
+    bonus: null, // 보너스 공
+    redo: false,
+  };
+
+  timeouts = [];
+
+  runTimeouts = () => {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        // setTimeout은 비동기 처리되어 태스큐에 쌓여있음 그래서 파바박 다 돌고 callstack으로 하나씩 이동해서 실행되는것임
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeouts();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [], // 당첨 번호들
+      bonus: null, // 보너스 공
+      redo: false,
+    });
+    this.timeouts = [];
+  };
 
   render() {
+    const { winBalls, bonus, redo } = this.state;
     return (
       <>
-        <div></div>
+        <div>당첨 숫자</div>
+        <div id="경과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+            //계속 추가된다고 생각하는데 그게 아니라 그냥 새롭게 다시 그리는것임!
+            //한 줄 그렸다가 다시 두 줄 새로 그렸다가, 다시 세 줄 새로 그렸다가, 다시 네 줄... 이런식임!!
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {/* 위에 당첨 번호 6개도 새로 그리고 거기에 추가로 보너스 번호까지 새로 그리고! 이것임 */}
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
       </>
     );
   }
