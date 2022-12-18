a82cb49a5e8fd559033368de05c95c07c0466276
WooDaeHyun <po668312@naver.com>
2022-12-18 13:42:28 +0900

    feat: Tic-Tac-Toe 게임 기본 구조 만들기 & useReducer와 props 사용

---
+++ 7.틱택톡/TicTacToe.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useReducer, useCallback } from "react";
+import Table from "./Table";
+
+const initialState = {
+  winner: "",
+  turn: "O",
+  tableData: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+};
+
+export const SET_WINNER = "SET_WINNER";
+export const CLICK_CELL = "CLICK_CELL";
+export const SET_TURN = "SET_TURN";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      };
+    case CLICK_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...tableData[action.row]]; // 나중에 immer라는 라이브러리로 가독성 해결함
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case SET_TURN: {
+      return {
+        ...state,
+        turn: state.turn === "O" ? "X" : "O",
+      };
+    }
+  }
+};
+
+const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [winner, setWinner] = useState("");
+  // const [turn, setTurn] = useState("O");
+  // const [tableData, setTableData] = useState([
+  //   ["", "", ""],
+  //   ["", "", ""],
+  //   ["", "", ""],
+  // ]);
+
+  const onClickTable = useCallback(() => {
+    dispatch({ type: SET_WINNER, winner: "O" });
+  }, []);
+  //쉽게 컴포넌트에 넣는 함수들은 useCallback 사용한다고 보면 됨
+  return (
+    <>
+      <Table
+        onClick={onClickTable}
+        tableData={state.tableData}
+        dispatch={dispatch}
+      />
+      {state.winner && <div>{state.winner}님의 승리</div>}
+    </>
+  );
+};
+
+export default TicTacToe;
